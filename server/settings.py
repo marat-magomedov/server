300cf2e5eacb73d9a5e7cc01d311a6b4794a2de7
marat-magomedov <imagomedov.marat@gmail.com>
2025-06-19 18:15:24 +0300

    fixes 6.0

--- server/settings.py
+++ server/settings.py
@@ -10,6 +10,10 @@ DEBUG = os.getenv('DEBUG', 'True')
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://marat-magomedov-server-6143.twc1.net",
+]
+
 YOOKASSA_SHOP_ID = os.getenv('YOOKASSA_SHOP_ID', 'your_shop_id')
 YOOKASSA_SECRET_KEY = os.getenv('YOOKASSA_SECRET_KEY', 'your_secret_key')
 
